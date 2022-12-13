5ab4b5c5033b8dd7430e3baa0154c67ccd1c268f
Beggle <75349776+Beggle@users.noreply.github.com>
2022-12-13 15:51:22 +1100

    changes

--- Euc_PMM/euc_PMM_example.R
+++ Euc_PMM/euc_PMM_example.R
@@ -7,22 +7,6 @@ library(surface);library(ouch);library(geiger);library(igraph);library(Rphylopar
 require(dplyr);library(brms);library(future)
 
 
-force.ultrametric<-function(tree,method=c("nnls","extend")){
-  method<-method[1]
-  if(method=="nnls") tree<-nnls.tree(cophenetic(tree),tree,
-                                     rooted=TRUE,trace=0)
-  else if(method=="extend"){
-    h<-diag(vcv(tree))
-    d<-max(h)-h
-    ii<-sapply(1:Ntip(tree),function(x,y) which(y==x),
-               y=tree$edge[,2])
-    tree$edge.length[ii]<-tree$edge.length[ii]+d
-  } else 
-    cat("method not recognized: returning input tree\n\n")
-  tree
-}
-
-
 ### DATA AND TREES ####
 
 # d.master <- read.csv("d.master.csv")
@@ -124,19 +108,22 @@ d.sub[is.na(d.sub$se.leaf_delta13C),]$se.leaf_delta13C <- quantile(d.sub[!is.na(
 # log-transform SLA and compute first-order standard error estimates (i.e. relative error) via delta method (se.SLA/SLA)
 d.sub <- d.sub %>% as_tibble %>% mutate(log_SLA = log(SLA), se_log_SLA = se.SLA/SLA,
                                         N = leaf_N_per_dry_mass, se_N = se.leaf_N_per_dry_mass,
-                                        d13C = leaf_delta13C, se_d13C = se.leaf_delta13C)
+                                        d13C = leaf_delta13C, se_d13C = se.leaf_delta13C,
+                                        N_area = leaf_N_per_dry_mass/SLA)
+
 # subset to relevant columns
-d.sub2 <- d.sub %>% select(animal, log_SLA, se_log_SLA,N, se_N,d13C,se_d13C)
+d.sub2 <- d.sub %>% select(animal, log_SLA, se_log_SLA,N, se_N,d13C,se_d13C, N_area)
 
 
 # ## SAVED FITS
-# b.0 <- readRDS("b.0.rds")
-# b.1 <- readRDS("b.1.rds")
-# b.2 <- readRDS("b.2.rds")
-# b.3 <- readRDS("b.3.rds")
+b.0 <- readRDS("b.0.rds")
+b.1 <- readRDS("b.1.rds")
+b.2 <- readRDS("b.2.rds")
+b.3 <- readRDS("b.3.rds")
 
 ## brms
 A.mat <- ape::vcv.phylo(phy1, corr = T)
+
 
 ## MOD 0 - raw trait scale
 bf_y1 <- bf(SLA | resp_se(se.SLA, sigma = TRUE) ~ 1 + (1|b|gr(animal, cov = A)))
@@ -205,13 +192,33 @@ b.3 <- brm(bf_y1 + bf_y2 + bf_y3 + set_rescor(TRUE),
 saveRDS(b.3, file = "b.3.rds")
 
 
+
+
+# calculate h2 
+mod <- b.1
+mean(unlist(purrr::map(mod$fit@sim$samples, ~.x$sd_animal__logSLA_Intercept))/(unlist(purrr::map(mod$fit@sim$samples, ~.x$sd_animal__logSLA_Intercept))+unlist(purrr::map(mod$fit@sim$samples, ~.x$sigma_logSLA))))
+quantile(unlist(purrr::map(mod$fit@sim$samples, ~.x$sd_animal__logSLA_Intercept))/(unlist(purrr::map(mod$fit@sim$samples, ~.x$sd_animal__logSLA_Intercept))+unlist(purrr::map(mod$fit@sim$samples, ~.x$sigma_logSLA))), probs = c(0.025, 0.975))
+
+
 ## COMBINE MODELS ##
 
 ## results similar across 3 available trees so combine
 # b.mod <- combine_models(b.1, b.2, b.3)
 # saveRDS(b.mod, file = "b.mod.rds")
 b.mod <- readRDS(file = "b.mod.rds")
-b.mod
+
+
+# calculate h2 (b.mod for combined chains across trees)
+mod <- b.1
+mean(unlist(purrr::map(mod$fit@sim$samples, ~.x$sd_animal__logSLA_Intercept))/(unlist(purrr::map(mod$fit@sim$samples, ~.x$sd_animal__logSLA_Intercept))+unlist(purrr::map(mod$fit@sim$samples, ~.x$sigma_logSLA))))
+quantile(unlist(purrr::map(mod$fit@sim$samples, ~.x$sd_animal__logSLA_Intercept))/(unlist(purrr::map(mod$fit@sim$samples, ~.x$sd_animal__logSLA_Intercept))+unlist(purrr::map(mod$fit@sim$samples, ~.x$sigma_logSLA))), probs = c(0.025, 0.975))
+
+mean(unlist(purrr::map(mod$fit@sim$samples, ~.x$sd_animal__N_Intercept))/(unlist(purrr::map(mod$fit@sim$samples, ~.x$sd_animal__N_Intercept))+unlist(purrr::map(mod$fit@sim$samples, ~.x$sigma_N))))
+quantile(unlist(purrr::map(mod$fit@sim$samples, ~.x$sd_animal__N_Intercept))/(unlist(purrr::map(mod$fit@sim$samples, ~.x$sd_animal__N_Intercept))+unlist(purrr::map(mod$fit@sim$samples, ~.x$sigma_N))), probs = c(0.025, 0.975))
+
+mean(unlist(purrr::map(mod$fit@sim$samples, ~.x$sd_animal__d13C_Intercept))/(unlist(purrr::map(mod$fit@sim$samples, ~.x$sd_animal__d13C_Intercept))+unlist(purrr::map(mod$fit@sim$samples, ~.x$sigma_d13C))))
+quantile(unlist(purrr::map(mod$fit@sim$samples, ~.x$sd_animal__d13C_Intercept))/(unlist(purrr::map(mod$fit@sim$samples, ~.x$sd_animal__d13C_Intercept))+unlist(purrr::map(mod$fit@sim$samples, ~.x$sigma_d13C))), probs = c(0.025, 0.975))
+
 
 # worth fitting a skew normal to SLA?
 p1 <- pp_check(b.mod, resp = "logSLA", ndraws = 100) + ggtitle("SLA") # prediction poor for SLA
@@ -221,6 +228,203 @@ ggarrange(p1,p2,p3, nrow = 1, ncol = 3)
 
 
 
+## PLOTTING ####
+
+library(tidybayes)
+
+b.mod$fit %>%
+  gather_draws(cor_animal__logSLA_Intercept__N_Intercept,
+               cor_animal__logSLA_Intercept__d13C_Intercept,
+               cor_animal__N_Intercept__d13C_Intercept,
+               rescor__logSLA__N,
+               rescor__logSLA__d13C,
+               rescor__N__d13C) %>%
+  median_hdi(.width = c(0.95, 0.5)) %>% 
+  mutate(cor = rep(rep(c("SLA ~ d13C","SLA ~ N_mass","N_mass ~ d13C"),2),2)) %>% 
+  mutate(level = rep(rep(c("phy","ind"),each=3),2)) %>%
+  ggplot(aes(y = cor, x = .value, xmin = .lower, xmax = .upper, col = level)) +
+  geom_pointinterval(position = position_dodge(width = 0.25)) +
+  geom_vline(xintercept = 0, col = "grey70", lty = "longdash") +
+  theme_classic() + 
+  theme(axis.text = element_text(size = 12),
+        axis.title = element_text(size = 14),
+        legend.text = element_text(size = 12),
+        legend.title = element_text(size = 14)) +
+  xlab("correlation estimate") + ylab("") + xlim(-1,1) +
+  scale_color_manual(values = c("red", "#0072B2")) + 
+  scale_y_discrete(labels=c("SLA ~ N_mass" = bquote(SLA~"~"~N[mass]),
+                            "SLA ~ d13C" = bquote(SLA~"~"~delta^13*C),
+                            "N_mass ~ d13C" = bquote(N[mass]~"~"~delta^13*C)))
+
+b.N %>%
+  gather_draws(cor_animal__logSLA_Intercept__Narea_Intercept,
+               cor_animal__logSLA_Intercept__d13C_Intercept,
+               cor_animal__Narea_Intercept__d13C_Intercept,
+               rescor__logSLA__Narea,
+               rescor__logSLA__d13C,
+               rescor__Narea__d13C) %>%
+  median_hdi(.width = c(0.95, 0.5)) %>% 
+  mutate(cor = rep(rep(c("SLA ~ d13C","SLA ~ N_area","N_area ~ d13C"),2),2)) %>% 
+  mutate(level = rep(rep(c("phy","res"),each=3),2)) %>%
+  ggplot(aes(y = cor, x = .value, xmin = .lower, xmax = .upper, col = level)) +
+  geom_pointinterval(position = position_dodge(width = 0.25)) +
+  geom_vline(xintercept = 0, col = "grey70", lty = "longdash") +
+  theme_classic() + 
+  theme(axis.text = element_text(size = 12),
+        axis.title = element_text(size = 14),
+        legend.text = element_text(size = 12),
+        legend.title = element_text(size = 14)) +
+  xlab("correlation estimate") + ylab("") + xlim(-1,1) +
+  scale_color_manual(values = c("black", blues9[6])) + 
+  scale_y_discrete(labels=c("SLA ~ N_area" = bquote(SLA~"~"~N[area]),
+                            "SLA ~ d13C" = bquote(SLA~"~"~delta^13*C),
+                            "N_area ~ d13C" = bquote(N[area]~"~"~delta^13*C)))
+
+
+
+## MULTI PANEL PLOT
+
+
+reps <- 3 # fixed for now
+N <- 30
+rho_b = 0.9
+rho_c = 0
+sig_b = 1
+sig_c = 1
+C <- matrix(c(sig_c,rho_c,rho_c,sig_c),2,2) # residual covariance matrix
+B <- matrix(c(sig_b,rho_b,rho_b,sig_b),2,2) # phylogenetic covariance matrix
+C.scale <- 1; C <- C*C.scale
+B.scale <- 1; B <- B*B.scale
+blength <- 0.1
+
+
+cols <- c("#E69F00","#56B4E9")
+
+p1.1 <- ggplot(d.sub, aes(N,log_SLA)) + 
+  geom_point(aes(col = subgenus)) + 
+  theme_classic() +
+  theme(legend.position = "none",
+        plot.margin = margin(0.25,0.2,0.2,0.675, "cm")) +
+  scale_color_manual(values=cols) +
+  ylab("log SLA")
+p1.2 <- ggplot(d.sub, aes(d13C,log_SLA)) + 
+  geom_point(aes(col = subgenus)) + 
+  theme_classic() +
+  theme(legend.position = "none",
+        plot.margin = margin(0.25,0.2,0.2,0.675, "cm")) +
+  scale_color_manual(values=cols) +
+  xlab(bquote(delta^13*C)) +
+  ylab("log SLA")
+p1.3 <- ggplot(d.sub, aes(N,d13C)) + 
+  geom_point(aes(col = subgenus)) + 
+  theme_classic() +
+  theme(legend.position = "none",
+        plot.margin = margin(0.25,0.2,0.2,0.25, "cm")) +
+  scale_color_manual(values=cols) +
+  ylab(bquote(delta^13*C))
+
+
+
+# # 3D scatter plot
+# library(plotly)
+# fig <- plot_ly(d.sub, x = ~log_SLA, y = ~N, z = ~d13C, color = ~subgenus, colors = c('#BF382A', '#0C4B8E'), size = I(50))
+# fig <- fig %>% add_markers()
+# fig <- fig %>% layout(scene = list(xaxis = list(title = 'SLA (log)'),
+#                                    yaxis = list(title = 'leaf N'),
+#                                    zaxis = list(title = 'd13C')))
+# fig
+
+
+
+# PLOT 2
+p2 <- ggtree(phy2)
+p2 <- p2 + theme(plot.margin = margin(1,0,1.2,0.1, "cm"))
+clade.lab <- unique(d.sub$subgenus)
+grp <- list()
+for(i in 1:length(clade.lab)){grp[[i]] = d.sub[d.sub$subgenus==clade.lab[1],]$taxon}
+names(grp) <-  unique(d.sub$subgenus)
+
+p2 <- groupOTU(p2, grp, 'clade') + aes(color=clade) + 
+  theme(legend.position = c(0.4,0.975)) +
+  # geom_tiplab(hjust = 0, align=TRUE, size=2) +
+  scale_color_manual(values=cols,
+                     name = "subgenus", labels = c("Eucalyptus", "Symphyomyrtus"))
+
+# p2 + geom_tiplab(aes(label=node), size=2, offset = 0.05)
+# p2.f <-  flip(p2, 92,151)
+# p2.f + geom_tiplab(aes(label=node), size=2, offset = 0.05)
+
+# PLOT 3
+target <- get_taxa_name(p2)
+d2 <- d.sub[match(target, d.sub$taxon),]
+d2 <- data.frame(
+  label = rep(d2$taxon,3),
+  trait = rep(c("SLA","N","d13C"), each=length(phy1$tip.label)),
+  value = c(scale(d2$log_SLA),scale(d2$N),scale(d2$d13C)))
+d2$label <- factor(d2$label, level = get_taxa_name(p2)) # re-order to match ggtree
+
+d2[d2$value==max(d2$value),]$value <- 5
+
+p3 <- ggplot(d2, aes(x=trait, y=label)) + 
+  geom_tile(aes(fill=value)) + 
+  # scale_fill_gradientn(limits=c(-2,2),
+  #                      colours = c("cyan2", "white", "orange2"),
+  #                      values = scales::rescale(c(-0.5, -0.2, 0, 0.2, 0.5))) +
+  scale_fill_viridis_c(name = "xyz",
+                       # limits = c(-2.5, 2.5),
+                       # breaks = c(-2, -1, 0, 1, 2))
+  ) + theme_minimal() + xlab(NULL) + ylab(NULL) +
+  scale_x_discrete(position = "top",
+                   labels=c("d13C" = bquote(delta^13*C))) +
+  scale_y_discrete(limits=rev(levels(d2$label))) +
+  # scale_x_discrete(expand=c(0,0)) +
+  theme(axis.text.y = element_blank(),
+        axis.text.x = element_text(size=9),
+        # axis.text.x = element_blank(),
+        panel.grid = element_blank(),
+        plot.margin = margin(0.4,0,1.3,0.1, "cm"),
+        # plot.margin = margin(1,0,1.2,0.1, "cm"),
+        legend.title = element_blank(),
+        legend.key.size = unit(0.5, 'cm'))
+
+pA <- cowplot::plot_grid(p1.1, p1.2, p1.3, axis = "l", nrow=3)
+pB <- cowplot::plot_grid(p2, p3, ncol=2, rel_widths = c(1,0.75))
+
+cowplot::plot_grid(pA, pB, nrow=1, rel_widths = c(0.65,1))
+
+
+## END ####
+
+
+## PREDICTION
+
+# choose fit
+pp <- predict(b.mod)
+
+str(pp)
+pSLA <- pp[,1,1]
+pN <- pp[,1,2]
+pd13C <- pp[,1,3]
+
+d.sub <- d.sub %>% mutate(SLA_res = d.sub$SLA - pp[,1,1])
+d.sub <- d.sub %>% mutate(N_res = d.sub$N - pp[,1,2])
+d.sub <- d.sub %>% mutate(d13C_res = d.sub$d13C - pp[,1,3])
+
+## while there is no relationship between N_mass and N_area, there is a significant
+# positive relationship between the residuals of N_mass (i.e., after accounting for phylogeny) 
+# and N_area (strengthened by removing outlier). Therefore, the positive residual correlation between
+# N_mass and d13C partly reflects co-variation between N_area and d13C, for which we predict a positive
+# relationship reflecting lower isotope discrimination with higher carboxylation capacity.
+plot(d.sub$N, d.sub$N_area); cor.test(d.sub$N, d.sub$N_area);abline(lm(N_area ~ N, data = d.sub))
+plot(d.sub$N_res, d.sub$N_area); cor.test(d.sub$N_res, d.sub$N_area);abline(lm(N_area ~ N_res, data = d.sub))
+plot(d.sub[d.sub$N_res<10,]$N_res, d.sub[d.sub$N_res<10,]$N_area); cor.test(d.sub[d.sub$N_res<10,]$N_res, d.sub[d.sub$N_res<10,]$N_area);abline(lm(N_area ~ N_res, data = d.sub[d.sub$N_res<10,]))
+
+
+plot(d.sub$N_res, d.sub$log_SLA); cor.test(d.sub$N_res, d.sub$log_SLA);abline(lm(log_SLA ~ N_res, data = d.sub))
+
+
+plot(log(d.sub$SLA), log(d.sub$N));cor.test(log(d.sub$SLA), log(d.sub$N))
+plot(log(d.sub$SLA), log(d.sub$N_area));cor.test(log(d.sub$SLA), log(d.sub$N_area))
 
 ## BRMS Multiple ##
 
--- Euc_PMM/euc_PMM_validation.R
+++ Euc_PMM/euc_PMM_validation.R
@@ -7,10 +7,9 @@ library(ggpubr)
 library(purrr)
 
 
-
 rm(list = ls())
 #fit <- readRDS("PMM_brms_2022_02_08.rds")
-fit <- readRDS("b.mod.rds")
+fit <- readRDS("b.1.rds")
 
 select <- dplyr::select
 
@@ -36,9 +35,9 @@ rstan::check_hmc_diagnostics(fit$fit)
 #----------------------------
 
 # prediction conditional on species-level random-intercept estimates
-ppA1 <- pp_check(fit, resp= "logSLA", type = "intervals") + labs(subtitle = "Response: logSLA")
-ppA2 <- pp_check(fit, resp= "N", type = "intervals") + labs(subtitle = "Response: N")
-ppA3 <- pp_check(fit, resp= "d13C", type = "intervals") + labs(subtitle = "Response: d13C")
+ppA1 <- pp_check(fit, resp= "logSLA", type = "intervals") + aes(x=reorder(factor(x), m)) + labs(x="taxon", subtitle = "SLA") + theme(axis.text.x = element_blank(), axis.ticks = element_blank())
+ppA2 <- pp_check(fit, resp= "N", type = "intervals") + aes(x=reorder(factor(x), m)) + labs(x="taxon", subtitle = "N") + theme(axis.text.x = element_blank(), axis.ticks = element_blank())
+ppA3 <- pp_check(fit, resp= "d13C", type = "intervals") + aes(x=reorder(factor(x), m)) + labs(x="taxon", subtitle = "d13C")  + theme(axis.text.x = element_blank(), axis.ticks = element_blank())
 
 ggarrange(ppA1, ppA2, ppA3, nrow = 3, legend = "none") %>% 
   annotate_figure(top = "PP-check: conditional on species-level random-intercept estimates")
@@ -48,10 +47,11 @@ get_coverage <- function(data) data %>%
             cov50 = (h >= y_obs) & (l <= y_obs)) %>% 
   map_dbl(sum)
 
-get_coverage(ppA1$data)
-get_coverage(ppA2$data)
-get_coverage(ppA3$data)
+get_coverage(ppA1$data)/length(fit$data$animal)
+get_coverage(ppA2$data)/length(fit$data$animal)
+get_coverage(ppA3$data)/length(fit$data$animal)
 
+## Re-order by pred mean
 # fixed effects only (i.e., ignore random effects by setting them to zero)
 ppB1 <- pp_check(fit, resp= "logSLA", type = "intervals", re_formula = NA) + labs(subtitle = "Response: logSLA")
 ppB2 <- pp_check(fit, resp= "N", type = "intervals", re_formula = NA) + labs(subtitle = "Response: N")
@@ -60,9 +60,9 @@ ppB3 <- pp_check(fit, resp= "d13C", type = "intervals", re_formula = NA) + labs(
 ggarrange(ppB1, ppB2, ppB3, nrow = 3, legend = "none") %>% 
   annotate_figure(top = "PP-check: fixed effects only (i.e., ignore random effects by setting them to zero)")
 
-get_coverage(ppB1$data)
-get_coverage(ppB2$data)
-get_coverage(ppB3$data)
+get_coverage(ppB1$data)/length(fit$data$animal)
+get_coverage(ppB2$data)/length(fit$data$animal)
+get_coverage(ppB3$data)/length(fit$data$animal)
 
 # prediction marginalised over Gaussian distribution of random effects
 pp1 <- pp_check(fit, resp= "logSLA", type = "intervals", 
@@ -80,20 +80,81 @@ pp3 <- pp_check(fit, resp= "d13C", type = "intervals",
                 sample_new_levels = "gaussian",
                 newdata = fit$data %>% mutate(animal = NA))
 
-ggarrange(
-  pp1 + labs(subtitle = "Response: logSLA"),
-  pp2 + labs(subtitle = "Response: N"),
-  pp3 + labs(subtitle = "Response: d13C"),
-  nrow = 3, legend = "none") %>% 
-  annotate_figure(top = "PP-check: marginalised over Gaussian distribution of random effects")
+dat1 <- ppA1$data
+dat2 <- ppA2$data
+dat3 <- ppA3$data
+pp1 <- pp1 + aes(x=reorder(factor(x), dat1$m)) + 
+  labs(subtitle = "SLA", x ="taxon") + theme(axis.text.x = element_blank(), 
+                                          axis.ticks = element_blank())
+pp2 <- pp2 + aes(x=reorder(factor(x), dat2$m)) + 
+  labs(subtitle = "N", x = "taxon") + theme(axis.text.x = element_blank(),
+                                          axis.ticks = element_blank())
+pp3 <- pp3 + aes(x=reorder(factor(x), dat3$m)) + 
+  labs(subtitle =  bquote(delta^13*C), x="taxon") + theme(axis.text.x = element_blank(),
+                                          axis.ticks = element_blank())
 
-pp1$data %>% get_coverage()
-pp2$data %>% get_coverage()
-pp3$data %>% get_coverage()
+ggarrange(
+  pp1,pp2,pp3,
+  nrow = 3, legend = "none") #%>% 
+  #annotate_figure(top = "PP-check: marginalised over Gaussian distribution of random effects")
+
+pp1$data %>% get_coverage()/length(fit$data$animal)
+pp2$data %>% get_coverage()/length(fit$data$animal)
+pp3$data %>% get_coverage()/length(fit$data$animal)
+
+
+bquote(SLA~"~"~delta^13*C)
+
+pp1 <- pp_check(fit, resp= "logSLA", type = "intervals", allow_new_levels = T, sample_new_levels = "gaussian",newdata = fit$data %>% mutate(animal = NA)) +
+  aes(x=reorder(factor(x), dat1$m)) + labs(x="taxon",y="log SLA") + theme(axis.title.x = element_blank(), axis.text.x = element_blank(), axis.ticks = element_blank(), axis.title.y = element_text(angle=0, vjust=0.5))
+pp2 <- pp_check(fit, resp= "N", type = "intervals", allow_new_levels = T, sample_new_levels = "gaussian",newdata = fit$data %>% mutate(animal = NA)) +
+  ylim(4,29) + aes(x=reorder(factor(x), dat2$m)) + labs(x="taxon",y="N") + theme(axis.title.x = element_blank(), axis.text.x = element_blank(), axis.ticks = element_blank(), axis.title.y = element_text(angle=0, vjust=0.5))
+pp3 <- pp_check(fit, resp= "d13C", type = "intervals", allow_new_levels = T, sample_new_levels = "gaussian",newdata = fit$data %>% mutate(animal = NA)) +
+  aes(x=reorder(factor(x), dat3$m)) + labs(x="taxon", y=bquote(delta^13*C)) + theme(axis.title.x = element_blank(), axis.text.x = element_blank(), axis.ticks = element_blank(), axis.title.y = element_text(angle=0, vjust=0.5))
+
+ppA1 <- pp_check(fit, resp= "logSLA", type = "intervals") + aes(x=reorder(factor(x), m)) + labs(x="taxon") + theme(axis.title.x = element_blank(), axis.text.x = element_blank(), axis.ticks = element_blank())
+ppA2 <- pp_check(fit, resp= "N", type = "intervals") + ylim(4,29) + aes(x=reorder(factor(x), m)) + labs(x="taxon") + theme(axis.title.x = element_blank(), axis.text.x = element_blank(), axis.ticks = element_blank())
+ppA3 <- pp_check(fit, resp= "d13C", type = "intervals") + aes(x=reorder(factor(x), m)) + labs(x="taxon")  + theme(axis.title.x = element_blank(), axis.text.x = element_blank(), axis.ticks = element_blank())
+
+ppA1 <- ppA1 + labs(subtitle = paste("94.8% at 90th quantile")) + theme(plot.subtitle = element_text(size = 8, vjust=-7, hjust=0.05))
+ppA2 <- ppA2 + labs(subtitle = paste("85.9% at 90th quantile")) + theme(plot.subtitle = element_text(size = 8, vjust=-7, hjust=0.05))
+ppA3 <- ppA3 + labs(subtitle = paste("85.6% at 90th quantile")) + theme(plot.subtitle = element_text(size = 8, vjust=-7, hjust=0.05))
+
+pp1 <- pp1 + labs(y = "", subtitle = paste("98% at 90th quantile")) + theme(plot.subtitle = element_text(size = 8, vjust=-7, hjust=0.05))
+pp2 <- pp2 + labs(y = "", subtitle = paste("97.4% at 90th quantile")) + theme(plot.subtitle = element_text(size = 8, vjust=-7, hjust=0.05))
+pp3 <- pp3 + labs(y = "", subtitle = paste("98.7% at 90th quantile")) + theme(plot.subtitle = element_text(size = 8, vjust=-7, hjust=0.05))
+
+ann1 <- ggplot() + geom_text(aes(x=0, y=0, label = "SLA"), 
+            parse = TRUE, size = 5, hjust = 0.5) + theme_void()
+ann2 <- ggplot() + geom_text(aes(x=0, y=0, label = "N"), 
+                             parse = TRUE, size = 5, hjust = 0) + theme_void()
+ann3 <- ggplot() + geom_text(aes(x=0, y=0, label = "delta^13*C"), 
+                             parse = TRUE, size = 5, hjust = 0.5) + theme_void()
+
+fig <- ggarrange(ann1,pp1,ppA1,
+                 ann2,pp2,ppA2,
+                 ann3,pp3,ppA3, nrow=3, ncol=3,
+                 widths = c(0.4,1,1),
+                 legend = "none", align = "hv")
+
+annotate_figure(fig,
+                top = text_grob("Marginal                                Conditional",
+                face = "bold", size = 15, rot=0, hjust = 0.3),
+                bottom = text_grob("taxon                                                    taxon",
+                                   size = 12, vjust=-0.25, hjust = 0.275))
+
+
 
 #----------------
 # loo predictions
 #----------------
+
+# new approach
+m.new <- fit %>% update(save_pars = save_pars(all = TRUE))
+future::plan(multisession(workers = 10))
+m.loo <- loo(m.new, moment_match = T, cores = 10, save_psis = T, reloo = T, future = T)
+loo_predict(m, psis = m.loo$psis_object, type = "quantile", probs = c(0.025, 0.25, 0.5, 0.75, 0.975))
+
 
 # draw posterior samples
 ppred <- posterior_predict(fit)
@@ -106,13 +167,14 @@ ppred3 <- ppred[,,3] # d13C
 log_ratios1 <- -1*log_lik(fit, resp = "logSLA")
 log_ratios2 <- -1*log_lik(fit, resp = "N")
 log_ratios3 <- -1*log_lik(fit, resp = "d13C")
-r_eff1 <- loo::relative_eff(exp(-log_ratios1), chain_id = rep(1:12, each = 1000))
-r_eff2 <- loo::relative_eff(exp(-log_ratios2), chain_id = rep(1:12, each = 1000))
-r_eff3 <- loo::relative_eff(exp(-log_ratios3), chain_id = rep(1:12, each = 1000))
+r_eff1 <- loo::relative_eff(exp(-log_ratios1), chain_id = rep(1:4, each = 1000))
+r_eff2 <- loo::relative_eff(exp(-log_ratios2), chain_id = rep(1:4, each = 1000))
+r_eff3 <- loo::relative_eff(exp(-log_ratios3), chain_id = rep(1:4, each = 1000))
 psis1 <- loo::psis(log_ratios1, cores = 10, r_eff = r_eff1)
 psis2 <- loo::psis(log_ratios2, cores = 10, r_eff = r_eff2)
 psis3 <- loo::psis(log_ratios3, cores = 10, r_eff = r_eff3)
 
+loo::E_loo
 # generate loo quantile predictions via PSIS
 loo_pred1 <- loo::E_loo(ppred1,psis_object = psis1, log_ratios = log_ratios1, type = "quantile", 
                         probs = c(0.05,0.25,0.5,0.75,0.95))
@@ -146,7 +208,7 @@ plot_loo_pred <- function(x,y_obs,cov, ylab = "y") x %>%
   geom_linerange(aes(ymin = l, ymax = h), size = 1, col = blues9[4]) +
   geom_point(aes(y = y_obs)) +
   theme_classic() +
-  labs(y = ylab, subtitle = paste("Coverage: ", cov[1], "% at 90th quantile, ",cov[2],"% at 50th quantile"),
+  labs(y = ylab, x = "taxon", subtitle = paste("Coverage: ", cov[1], "% at 90th quantile, ",cov[2],"% at 50th quantile"),
        title = "LOO-predictive checks")
 
 # main plot
